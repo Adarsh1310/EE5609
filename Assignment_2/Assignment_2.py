6604e193479235dc1c1a34e58a8ec4a2457b0b80
Adarsh Srivastava <adarsh.srivastava235@gmail.com>
2020-09-02 20:05:58 +0530

    Update Assignment_2.py

--- Assignment_2/Assignment_2.py
+++ Assignment_2/Assignment_2.py
@@ -1,9 +1,3 @@
-VecStart_x = [0,1,3,5]
-VecStart_y = [2,2,5,5]
-VecStart_z = [0,1,1,5]
-VecEnd_x = [1,2,-1,6]
-VecEnd_y = [3,1,-2,7]
-VecEnd_z  =[1,0,4,9]
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
