5eeb9f9df689620d6f9ba32795059d55cf0ff461
Adarsh Srivastava <adarsh.srivastava235@gmail.com>
2020-09-02 12:03:03 +0530

    Add files via upload

---
+++ codes/Assignment 2.py
@@ -0,0 +1,11 @@
+import numpy as py
+from sympy import Symbol, Eq,sqrt,solve,Point,Line
+import matplotlib.pyplot as plt
+x=1
+y=2
+z=3
+eqn1=(x-1)/3
+eqn2=(y-2)/2
+eqn3=(z-3)/-2
+if eqn1==eqn2==eqn3:
+    print ("Equation of the line is correct ")
