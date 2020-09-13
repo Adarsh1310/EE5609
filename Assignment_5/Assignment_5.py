71218082bc503487b61a4803f82a8fb82d3d193d
Adarsh Srivastava <adarsh.srivastava235@gmail.com>
2020-09-13 19:44:20 +0530

    Add files via upload

--- Assignment_5/Assignment_5.py
+++ Assignment_5/Assignment_5.py
@@ -1,23 +1,38 @@
+import numpy as np
 import matplotlib.pyplot as plt
+from numpy import linalg as LA
 import math
-
-circle1 = plt.Circle((0, 0), 2, fill=False)
-circle2 = plt.Circle((2, 0), 2, fill=False, color='b')
-
-fig, ax = plt.subplots()
-
-plt.xlim(-5, 5)
-plt.ylim(-5, 5)
-plt.scatter(1, math.sqrt(3))
-plt.scatter(1, -math.sqrt(3))
-plt.annotate("Point 1", (1, math.sqrt(3)))
-plt.annotate("Point 2", (1, -math.sqrt(3)))
-
-ax.set_aspect(1)
-
-ax.add_artist(circle1)
-ax.add_artist(circle2)
-
-plt.savefig("plot_circle_matplotlib_02.png", bbox_inches='tight')
+def circ_gen(O,r):
+	len = 50
+	theta = np.linspace(0,2*np.pi,len)
+	x_circ = np.zeros((2,len))
+	x_circ[0,:] = r*np.cos(theta)
+	x_circ[1,:] = r*np.sin(theta)
+	x_circ = (x_circ.T + O).T
+	return x_circ
+U_1 = np.array(([0,0]))
+U_2= np.array(([2,0]))
+P_1 = np.array(([1,math.sqrt(3)]))
+P_2 = np.array(([1,-math.sqrt(3)]))
+P_3 = np.array(([0,0]))
+P_4 = np.array(([2,0]))
+r_1 = np.sqrt(LA.norm(U_1)**2+4)
+r_2 = np.sqrt(LA.norm(U_2)**2-0)
+x_circ1= circ_gen(U_1,r_1)
+x_circ2=circ_gen(U_2,r_2)
+plt.plot(x_circ1[0,:],x_circ1[1,:],label='$circle with centre(0,0)$')
+plt.plot(x_circ2[0,:],x_circ2[1,:],label='$circle with centre(2,0)$')
+plt.scatter(1,math.sqrt(3))
+plt.scatter(1,-math.sqrt(3))
+plt.scatter(0,0)
+plt.scatter(2,0)
+plt.annotate("A", (1, math.sqrt(3)))
+plt.annotate("B", (1, -math.sqrt(3)))
+plt.annotate("O_1", (0,0))
+plt.annotate("O_2", (2,0))
+plt.plot([0,1],[0, math.sqrt(3)])
+plt.plot([0, 1],[0, -math.sqrt(3)])
+plt.plot([2,1],[0, math.sqrt(3)])
+plt.plot([2, 1],[0, -math.sqrt(3)])
 
 plt.show()
