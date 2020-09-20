63e606af27f450bbddb059a851ebfa9d7f90f1a6
Adarsh Srivastava <adarsh.srivastava235@gmail.com>
2020-09-20 17:38:31 +0530

    Update Assignment_8.py

--- Assignment_8/Assignment_8.py
+++ Assignment_8/Assignment_8.py
@@ -1,5 +1,5 @@
 import numpy as np
-Q=np.array([[-0.7071,0.7071],[0.7071,-0.7071]])
-R=np.array([[0.7071,-0.7071],[0,0]])
+Q=np.array([[-0.7071,0],[0.7071,0]])
+R=np.array([[0.7071,-0.7071],[0,0.707]])
 result=Q@R
 print(result)
